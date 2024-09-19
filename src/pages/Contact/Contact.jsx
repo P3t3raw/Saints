cf2a43ebe0439de4b3b17df69ec9a046bb56eebe
P3t3raw <peter.ookoth@gmail.com>
2024-09-19 14:56:21 +0300

    committing the addition of the booking appointment module

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,26 +1,41 @@
+import React, { useState } from "react";
 import "./Contact.css";
 import { Link } from "react-router-dom";
 import { FaChevronRight } from "react-icons/fa";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
-import React from "react";
+import churchImage from "../../assets/church1.jpg";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-const customIcon = new L.Icon({
-  iconUrl: require("../../assets/church1.jpg"), // Custom church icon image (change this path)
-  iconSize: [32, 32],
-  iconAnchor: [16, 32],
-  popupAnchor: [0, -32],
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
 });
 
 export default function Contact() {
+  const [selectedOption, setSelectedOption] = useState("");
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedTime, setSelectedTime] = useState("");
+
+  const handleOptionChange = (e) => {
+    setSelectedOption(e.target.value);
+  };
+
+  const handleTimeChange = (e) => {
+    setSelectedTime(e.target.value);
+  };
   const churchLocation = [-1.3076028456483761, 36.91171768994212];
   return (
     <div>
       <div className="aboutBackground">
         <div className="aboutOverlay"></div>
         <div className="aboutContent">
-          <h1>Get to Know Us</h1>
+          <h1>Contact Us</h1>
         </div>
         <div className="aboutBreadCrumbsWrapper">
           <p className="aboutBreadCrumbs">
@@ -28,69 +43,22 @@ export default function Contact() {
               <Link to="/">Home</Link> <FaChevronRight />
             </span>
             <span>
-              About <FaChevronRight />
+              Contact Us <FaChevronRight />
             </span>
           </p>
-          <h1>About</h1>
+          <h1>Contact Us</h1>
         </div>
       </div>
 
       <div className="contact-container">
-        <p>
-          We would love to hear from you. Please reach out to us through any of
-          the methods below.
-        </p>
-
-        <div className="contact-info">
-          <div className="contact-details">
-            <h2>Our Contact Information</h2>
-            <p>
-              <strong>Address:</strong> 123 Church Lane, City, Country
-            </p>
-            <p>
-              <strong>Phone:</strong> +123 456 7890
-            </p>
-            <p>
-              <strong>Email:</strong> info@churchname.org
-            </p>
-          </div>
-
-          <div className="contact-map">
-            {/* Placeholder for Google Maps */}
-            <h2>Find Us</h2>
-            <MapContainer
-              center={churchLocation}
-              zoom={13}
-              scrollWheelZoom={false}
-              style={{ height: "300px", width: "100%" }}
-            >
-              <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              />
-              <Marker position={churchLocation} icon={customIcon}>
-                <Popup>
-                  <div className="pin">
-                    <img
-                      className="img-style"
-                      src="../../assets/church1.jpg" // Assuming the first image is the primary one
-                      alt="church"
-                    />
-
-                    <div>
-                      <strong>ACK St. Peter's</strong>
-                    </div>
-                  </div>
-                </Popup>
-              </Marker>
-            </MapContainer>
-          </div>
-        </div>
-
         <div className="contact-form-container">
           <h2>Send Us a Message</h2>
           <form className="contact-form">
-            <select className="contact-select" required>
+            <select
+              className="contact-select"
+              required
+              onChange={handleOptionChange}
+            >
               <option value="" disabled selected>
                 Select message title
               </option>
@@ -108,13 +76,90 @@ export default function Contact() {
               <input type="email" placeholder="Your Email" required />
             </label>
 
-            <label>
-              Message:
-              <textarea placeholder="Your Message" required></textarea>
-            </label>
+            {selectedOption === "booking" ? (
+              <>
+                <label>Select Date:</label>
+                <DatePicker
+                  selected={selectedDate}
+                  onChange={(date) => setSelectedDate(date)}
+                  placeholderText="Pick a date"
+                  required
+                />
+                <label>Select Time:</label>
+                <select
+                  value={selectedTime}
+                  onChange={handleTimeChange}
+                  required
+                >
+                  <option value="" disabled selected>
+                    Select a time
+                  </option>
+                  <option value="09:00 AM">09:00 AM</option>
+                  <option value="10:00 AM">10:00 AM</option>
+                  <option value="11:00 AM">11:00 AM</option>
+                  <option value="01:00 PM">01:00 PM</option>
+                  <option value="02:00 PM">02:00 PM</option>
+                  <option value="03:00 PM">03:00 PM</option>
+                </select>
+              </>
+            ) : (
+              <label>
+                Message:
+                <textarea placeholder="Your Message" required></textarea>
+              </label>
+            )}
 
             <button type="submit">Submit</button>
           </form>
+        </div>
+        <div className="contact-info">
+          {/* <p>
+            We would love to hear from you. Please reach out to us through any
+            of the methods below.
+          </p> */}
+          <div className="contact-details">
+            <h2>Our Contact Information</h2>
+            <p>
+              <strong>Address:</strong> P.O Box 40539 - 13356, Nairobi
+            </p>
+            <p>
+              <strong>Phone:</strong> 0110008603
+            </p>
+            <p>
+              <strong>Email:</strong> info@ackstpetersembakasi.org
+            </p>
+          </div>
+
+          <div className="contact-map">
+            {/* Placeholder for Google Maps */}
+            <h2>Find Us</h2>
+            <MapContainer
+              center={churchLocation}
+              zoom={13}
+              scrollWheelZoom={false}
+              style={{ height: "300px", width: "100%" }}
+            >
+              <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              />
+              <Marker position={churchLocation}>
+                <Popup>
+                  <div className="pin">
+                    <img
+                      className="img-style"
+                      src={churchImage} // Assuming the first image is the primary one
+                      alt="church"
+                    />
+
+                    <div>
+                      <strong>ACK St. Peter's Embakasi</strong>
+                    </div>
+                  </div>
+                </Popup>
+              </Marker>
+            </MapContainer>
+          </div>
         </div>
       </div>
     </div>
